19807f79203b37bbb72e1f0e5b0711fe08ba2b46
coeit <cbeiteneuer@gmail.com>
2019-03-20 18:50:17 +0100

    Statistics

--- src/main/java/de/hhu/bsinfo/dxapp/MainPR.java
+++ src/main/java/de/hhu/bsinfo/dxapp/MainPR.java
@@ -124,18 +124,19 @@ public class MainPR extends AbstractApplication {
 
                 }
             }
-            for (short nodeID: computeService.getStatusMaster((short) 0).getConnectedSlaves()){
+            RoundVotes.add(votes);
+            /*for (short nodeID: computeService.getStatusMaster((short) 0).getConnectedSlaves()){
                 IntegerChunk integerChunk = new IntegerChunk(nameService.getChunkID(NodeID.toHexString(nodeID).substring(2,6),333));
                 chunkService.get().get(integerChunk);
                 //System.out.println(NodeID.toHexString(nodeID) + " votes: " + integerChunk.get_value());
                 votes += integerChunk.get_value();
             }
-            RoundVotes.add(votes);
+
             NumRounds++;
             if((double) votes / (double) N >= 0.9){
                 //System.out.println(">>Reached vote halting limit in round " + i);
                 break;
-            }
+            }*/
         }
         stopwatch.stop();
         //System.out.println("Timer Computation: " + stopwatch.getTimeStr());
