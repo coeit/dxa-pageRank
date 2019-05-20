f857e475368d401e3b1e69f8fd581daf68822e48
coeit <36563701+coeit@users.noreply.github.com>
2019-05-20 17:14:35 +0200

    Update MainPR.java

--- src/main/java/de/hhu/bsinfo/dxapp/MainPR.java
+++ src/main/java/de/hhu/bsinfo/dxapp/MainPR.java
@@ -8,7 +8,6 @@ import java.util.Date;
 import de.hhu.bsinfo.dxapp.chunk.VoteChunk;
 import de.hhu.bsinfo.dxmem.data.ChunkID;
 import de.hhu.bsinfo.dxram.app.AbstractApplication;
-import de.hhu.bsinfo.dxram.boot.BootService;
 import de.hhu.bsinfo.dxram.chunk.ChunkService;
 import de.hhu.bsinfo.dxram.engine.DXRAMVersion;
 import de.hhu.bsinfo.dxram.generated.BuildConfig;
@@ -18,12 +17,11 @@ import de.hhu.bsinfo.dxram.ms.*;
 import de.hhu.bsinfo.dxram.ms.script.TaskScript;
 import de.hhu.bsinfo.dxapp.tasks.*;
 import de.hhu.bsinfo.dxutils.Stopwatch;
-import de.hhu.bsinfo.dxram.nameservice.NameserviceService;
 
 /**
- * "Hello world" example DXRAM application
+ * PageRank implemetation for DXRAM
  *
- * @author Stefan Nothaas, stefan.nothaas@hhu.de, 17.05.17
+ * @author Constantin Eiteneuer, constantin.eiteneuer@hhu.de
  */
 public class MainPR extends AbstractApplication {
     @Override
@@ -53,31 +51,13 @@ public class MainPR extends AbstractApplication {
         boolean printPR = Boolean.parseBoolean(p_args[4]);
         boolean isSynthetic = false;
 
-
-        BootService bootService = getService(BootService.class);
         ChunkService chunkService = getService(ChunkService.class);
-        NameserviceService nameService = getService(NameserviceService.class);
         MasterSlaveComputeService computeService = getService(MasterSlaveComputeService.class);
         JobService jobService = getService(JobService.class);
 
         ArrayList<Short> connectedSlaves = computeService.getStatusMaster((short) 0).getConnectedSlaves();
 
-        /*IntegerChunk rdyCnt = new IntegerChunk();
-        chunkService.create().create(bootService.getNodeID(),rdyCnt);
-        chunkService.put().put(rdyCnt);*/
-        /*int k = 0;
-        IntegerChunk[] edgeChunks = new IntegerChunk[computeService.getStatusMaster((short) 0).getConnectedSlaves().size()];
-        for (short nodeID : computeService.getStatusMaster((short) 0).getConnectedSlaves()) {
-            IntegerChunk edgeCnt = new IntegerChunk();
-            chunkService.create().create(nodeID,edgeCnt);
-            chunkService.put().put(edgeCnt);
-            edgeChunks[k] = edgeCnt;
-            //System.out.println(voteChunks[k].getID() + " " + chunk.getPRsum());
-            k++;
-        }*/
-
         Stopwatch stopwatch = new Stopwatch();
-        System.out.println("len: "  + p_args.length);
 
         String filename = "SYNTHETIC";
         double locality = 0.0;
@@ -123,26 +103,10 @@ public class MainPR extends AbstractApplication {
         long memUsage = 0;
         long edgeCnt = 0;
         System.out.println("GRAPH INPUT DONE...");
-
-        /*VoteChunk voteChunk = new VoteChunk(N);
-        chunkService.create().create(bootService.getNodeID(),voteChunk);
-        chunkService.put().put(voteChunk);*/
         VoteChunk[] voteChunks = new VoteChunk[connectedSlaves.size()];
-        //long[] voteChunkIDs = new long[computeService.getStatusMaster((short) 0).getConnectedSlaves().size()];
-        int k = 0;
-        /*for (short nodeID : computeService.getStatusMaster((short) 0).getConnectedSlaves()) {
-            VoteChunk chunk = new VoteChunk(N);
-            chunkService.create().create(nodeID,chunk);
-            chunkService.put().put(chunk);
-            voteChunks[k] = chunk;
-            //System.out.println(voteChunks[k].getID() + " " + chunk.getPRsum());
-            k++;
-        }*/
 
         for (int i = 0; i < connectedSlaves.size(); i++) {
-            //System.out.println(ChunkID.getChunkID(connectedSlaves.get(i),localVertexCnt(N,i,connectedSlaves.size()) +1));
             voteChunks[i] = new VoteChunk(ChunkID.getChunkID(connectedSlaves.get(i),localVertexCnt(N,i,connectedSlaves.size()) + 1));
-            //System.out.println(voteChunks[i].getID());
             chunkService.get().get(voteChunks[i]);
             memUsage += chunkService.status().getStatus(connectedSlaves.get(i)).getHeapStatus().getUsedSize().getBytes();
             edgeCnt += voteChunks[i].getEdgeCnt();
@@ -151,17 +115,13 @@ public class MainPR extends AbstractApplication {
         System.out.println("VERTICES: " + N);
         System.out.println("EDGES: " + edgeCnt);
         System.out.println("Memory: " + memUsage + "B");
-        //System.out.println("nid: " + bootService.getNodeID() + " VERTEX COUNT: " + N);
 
         RunLumpPrRoundTask Run1 = new RunLumpPrRoundTask(N,DAMPING_FACTOR,0,false);
         RunLumpPrRoundTask Run2 = new RunLumpPrRoundTask(N,DAMPING_FACTOR,1,false);
 
-        //TaskScript taskScript = new TaskScript(Run1,Run2);
-
         TaskScript taskScriptRun1 = new TaskScript(Run1);
         TaskScript taskScriptRun2 = new TaskScript(Run2);
 
-        ArrayList<Double> roundPRsum = new ArrayList<>();
         ArrayList<Double> roundPRerr = new ArrayList<>();
 
         int NumRounds = 0;
@@ -169,15 +129,18 @@ public class MainPR extends AbstractApplication {
         double PRerr;
         ArrayList<Long> iterationTimes = new ArrayList<>();
         TaskScriptState state;
+
         for (int i = 0; i < MAX_ROUNDS; i++) {
             danglingPR = 1;
             PRerr = 0;
             stopwatch.start();
+
             if(i % 2 == 0){
                 state = computeService.submitTaskScript(taskScriptRun1, (short) 0);
             } else {
                 state = computeService.submitTaskScript(taskScriptRun2, (short) 0);
             }
+
             while (!state.hasTaskCompleted()) {
                 try {
                     Thread.sleep(100);
@@ -185,22 +148,19 @@ public class MainPR extends AbstractApplication {
 
                 }
             }
+
             chunkService.get().get(voteChunks);
+
             for (VoteChunk voteChunk : voteChunks) {
                 PRerr += voteChunk.getPRerr();
                 danglingPR = danglingPR - voteChunk.getPRsum();
             }
+
             for (VoteChunk voteChunk : voteChunks) {
                 voteChunk.setPRsum(danglingPR);
             }
+
             chunkService.put().put(voteChunks);
-            //System.out.println(danglingPR);
-            /*chunkService.get().get(voteChunk,ChunkLockOperation.WRITE_LOCK_ACQ_PRE_OP);
-            PRerr = voteChunk.getPRerr();
-            danglingPR = 1 - voteChunk.getPRsum(Math.abs(i % 2 - 1));
-            voteChunk.resetSum(i % 2, danglingPR);
-            voteChunk.resetErr();
-            chunkService.put().put(voteChunk,ChunkLockOperation.WRITE_LOCK_REL_POST_OP);*/
             stopwatch.stop();
 
             roundPRerr.add(PRerr);
@@ -221,6 +181,7 @@ public class MainPR extends AbstractApplication {
         RunLumpPrRoundTask calcDanglingPR = new RunLumpPrRoundTask(N,DAMPING_FACTOR,NumRounds % 2,true);
         TaskScript taskScriptCalcDanglingPR = new TaskScript(calcDanglingPR);
         state = computeService.submitTaskScript(taskScriptCalcDanglingPR,(short) 0);
+
         while (!state.hasTaskCompleted()) {
             try {
                 Thread.sleep(100);
@@ -244,22 +205,9 @@ public class MainPR extends AbstractApplication {
             }
         }
 
-
-        //double[] roundPRsumArr = roundPRsum.stream().mapToDouble(i -> i).toArray();
         double[] roundPRerrArr = roundPRerr.stream().mapToDouble(i -> i).toArray();
         long[] iterationTimesArr = iterationTimes.stream().mapToLong(i -> i).toArray();
 
-
-        /*int l = 0;
-
-        for (short slave : slaves){
-            memUsage += chunkService.status().getStatus(slave).getHeapStatus().getUsedSize().getMBDouble();
-            edgeCnt += voteChunks[l].getEdgeCnt();
-        }*/
-
-        //chunkService.get().get(edgeCnt);
-        //System.out.println("EdgeCnt:" + edgeCnt.get_value());
-        //System.out.println("EdgeCnt:" + edgeCnt);
         double memUseMB = (double) memUsage / Math.pow(1024,2);
         PrStatisticsJob prStatisticsJob = new PrStatisticsJob(outDir,filename,N,edgeCnt,DAMPING_FACTOR,THRESHOLD,inputTime,iterationTimesArr,memUseMB,roundPRerrArr,locality,meanInDeg);
         jobService.pushJobRemote(prStatisticsJob, computeService.getStatusMaster((short) 0).getConnectedSlaves().get(0));
